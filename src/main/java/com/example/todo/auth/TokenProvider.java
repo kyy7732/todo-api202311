ae1324ba8f9393a7e08cc84809ce53fdddc023da
yangwoo <kyy77321@naver.com>
2023-11-27 16:40:53 +0900

    # 4. User 회원가입 컨트롤러

---
+++ src/main/java/com/example/todo/auth/TokenProvider.java
@@ -0,0 +1,13 @@
+package com.example.todo.auth;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+// 역할: 토큰을 발급하고, 서명 위조를 검사하는 객체.
+public class TokenProvider {
+
+
+
+}
